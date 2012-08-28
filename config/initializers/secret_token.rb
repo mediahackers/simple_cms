fca107691fb2cbb8e2ad696644ae3d13c434f51c
Tim Kirby <mediahackers@gmail.com>
2012-08-28 10:32:54 +0100

    Initial Commit on demo controller created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleCms::Application.config.secret_token = '41c0fabc753bb12337d994d078320aadf681a1426153a0d5e5923fcc277df8f8c5aec9a08cccea777be09fde970d5c5a874d903ac8ffee8c4e34c0a45056874f'
